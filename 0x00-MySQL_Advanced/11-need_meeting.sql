752cb9b10eaf463d1001d13854d700d99f75b6e8
iradukundapaci <paifique@gmail.com>
2024-04-08 13:21:51 +0200

    editing task 11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,17 +1,4 @@
--- creates a stored procedure ComputeAverageWeightedScoreForUsers
--- that computes and stores the average weighted score for all students
-
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
-DELIMITER |
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
-BEGIN
-  UPDATE users AS U, 
-    (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg 
-    FROM users AS U 
-    JOIN corrections as C ON U.id=C.user_id 
-    JOIN projects AS P ON C.project_id=P.id 
-    GROUP BY U.id)
-  AS WA
-  SET U.average_score = WA.w_avg 
-  WHERE U.id=WA.id;
-END;
+-- Script that creates a view need_meeting that lists all students
+-- that have a score under 80 (strict) and no last_meeting or more than 1 month.
+CREATE VIEW need_meeting AS SELECT name from students WHERE score < 80
+AND (last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
