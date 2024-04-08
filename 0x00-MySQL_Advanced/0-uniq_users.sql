a09e018d3423dfba20992e2a8d8817f8f254e181
iradukundapaci <paifique@gmail.com>
2024-04-08 13:00:59 +0200

    turning  data type specifies to lower case

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 --Use any database create 
 --new users tables Drops existing table
 CREATE TABLE IF NOT EXISTS users (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255)
 );
