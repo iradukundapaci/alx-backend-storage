86ae5ea4db39d2b464f52de6478df068f2c21503
iradukundapaci <paifique@gmail.com>
2024-04-08 12:57:33 +0200

    testing querry

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
--- script that creates a table users with follwing fields
--- id, email, name
+--Use any database create 
+--new users tables Drops existing table
 CREATE TABLE IF NOT EXISTS users (
-	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
