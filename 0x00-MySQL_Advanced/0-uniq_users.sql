854cfe4799d90c9aaf5713be03992064033803f5
iradukundapaci <paifique@gmail.com>
2024-04-08 12:55:44 +0200

    more trial

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
---Use any database create new users tables Drops existing table
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+-- script that creates a table users with follwing fields
+-- id, email, name
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
 );
